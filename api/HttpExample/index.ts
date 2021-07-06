11a6f640b8e418a9505d108c1be173b6274b3c8f
Steve Drake <sdrake@truenorthit.co.uk>
2021-07-06 19:05:39 +0100

    getting user

--- api/HttpExample/index.ts
+++ api/HttpExample/index.ts
@@ -1,22 +1,21 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+declare const Buffer
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    
+
     context.log('HTTP trigger function processed a request.');
 
-
-    console.log(req.headers);
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const header = req.headers['x-ms-client-principal'];
+    const encoded = Buffer.from(header, 'base64');
+    const decoded = encoded.toString('ascii');
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body:
-            { responseMessage }
+        body: {
+            clientPrincipalx: JSON.parse(decoded),
+        },
     };
+
+
 
 };
 
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor( private httpClient: HttpClient) {}
+  async ngOnInit() {
+
+    var r = await this.httpClient.get('/api/HttpExample').toPromise();
+    console.log('r',r);
+
+    var r = await this.httpClient.get('/.auth/me').toPromise();
+    console.log('r',r);
+    
+  }
   title = 'AuthTest';
 }
+
+
